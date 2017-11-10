162d76a5a5c35e31deb7defc7125fcdf741e5df3
李俊峰 <lijunfeng@u51.com>
2017-11-10 14:45:02 +0800

    no message

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -27,3 +27,24 @@ rightChannel.currentLevel = 11
 print(rightChannel.currentLevel)
 print(AudioChannel.maxInputLevelForAllChannels)
 
+class Counter {
+    var count = 0
+    
+    func increment() {
+        count += 1
+    }
+    
+    func increment(by amount: Int) {
+        count += amount
+    }
+    
+    func reset() {
+        count = 0
+    }
+}
+
+let counter = Counter()
+counter.increment()
+counter.increment(by: 5)
+counter.reset()
+		
